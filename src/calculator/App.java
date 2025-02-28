678d163aa9e09048f2092a734d93e9baabc54b06
escomputer <nujest307@naver.com>
2025-02-28 17:39:23 +0900

    레벨 2까지 다해봄 ! 세터(리스트에 값 더하기) 게터(리스트목록가져오기)

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -5,53 +5,40 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
+        Calculator cal = new Calculator();
         Scanner sc = new Scanner(System.in);
-
-
 
         while(true){
             char op;
             int num1, num2;
-            try{System.out.print("연산자를 입력하세요 (+,-,*,-) : ");
+            try {
+                System.out.print("연산자를 입력하세요 (+,-,*,-) : ");
                 op = sc.next().charAt(0);
-                if (!(op == '+' || op == '-' || op == '*' || op == '/')) {
-                    System.out.println("잘못된 연산자입니다.");
-                    continue;
 
-                }
                 System.out.print("첫번째 정수를 입력하세요 : ");
                 num1 = sc.nextInt();
                 System.out.print("두번째 정수를 입력하세요 : ");
-                num2 = sc.nextInt();}
+                num2 = sc.nextInt();
 
+                double result = cal.calculate(num1, num2, op);
+                cal.addResult(result);
+                System.out.println(result);
+                System.out.println(cal.getResult());
+
+            }
             catch(InputMismatchException e){
                 System.out.println("정수를 입력해야합니다.");
                 sc.nextLine();
-                continue;
 
+            }catch(ArithmeticException e){
+                System.out.println("0으로 나눌 수 없습니다.");
+            }catch(IllegalArgumentException e){
+                System.out.println("잘못된 연산자입니다.");
             }
 
 
-            switch (op) {
-                case '+':
-                    System.out.println("결과 : " + (num1 + num2));
-                    break;
-                case '-':
-                    System.out.println("결과 : " + (num1 - num2));
-                    break;
-                case '*':
-                    System.out.println("결과 : " +(num1 * num2));
-                    break;
-                case '/':
-                    if (num2 == 0) {
-                        System.out.println("0으로 나눌 수 없습니다.");
-                    } else {
-                        System.out.println("결과 : " + (double) num1 / num2);
-                    }
-                    break;
 
-
-            }
 
             System.out.print("연산을 계속 하시겠습니까? ( exit 입력 시, 종료) : ");
             String answer = sc.next();
--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -1,8 +1,49 @@
 package calculator;
 
+
+import java.util.ArrayList;
+
 public class Calculator {
+    private static final ArrayList<Double> results = new ArrayList<>();
+    private static final int MAX_SIZE = 5;
 
-    public static void main(String[] args) {
+    public double calculate(int a, int b, char op) {
 
+
+        switch (op) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+
+            case '*':
+                return a * b;
+            case '/':
+                if (b == 0) {
+                    throw new ArithmeticException();
+                } else {
+                    return (double) a / b;
+                }
+            default:
+                throw new IllegalArgumentException();
+
+        }
     }
+
+    public void addResult(double result) {
+        results.add(result);
+        removeOldestResult();
+    }
+
+    public void removeOldestResult() {
+        if (results.size() > MAX_SIZE) {
+            results.remove(0);
+        }
+    }
+
+    public ArrayList<Double> getResult(){
+        return new ArrayList<>(results);
+    }
+
+
 }
